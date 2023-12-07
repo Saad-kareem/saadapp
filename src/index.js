e4d1fb0a9ebf55285ab777a052eeeb78f1de128b
Saadkareem12 <saadkareem475@gmail.com>
2023-12-07 22:55:51 +0500

    Add Initail

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let currDate = new Date().getHours();
+let gretting = "";
+const CssStyle = {};
+
+if (currDate >= 1 && currDate < 12) {
+  gretting = "Good Morning";
+  CssStyle.color = "green";
+} else if (currDate >= 12 && currDate < 19) {
+  gretting = "Good Afternon";
+  CssStyle.color = "Orange";
+} else {
+  gretting = "Good Night";
+  CssStyle.color = "#808080";
+}
+ReactDOM.render(
+  <>
+    <div>
+      <h1>
+        Hello Sir, <span style={CssStyle}> {gretting} </span>
+      </h1>
+    </div>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
